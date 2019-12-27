afa1b23633b56814afc7b1ff3455310ed15b5ae2
francesc <fxbullich@gmail.com>
2019-12-27 14:30:24 +0100

    [FEAT] S'ha afegit una capçalera per que no sigui necessaria BDD a Spring. S'han afegit paquets que no estaven al package.json necessaris per l'aplicació react

--- DockerService/src/main/java/vps/docker/DockerService/Security/WebSecurityConfiguration.java
+++ DockerService/src/main/java/vps/docker/DockerService/Security/WebSecurityConfiguration.java
@@ -1,12 +1,16 @@
 package vps.docker.DockerService.Security;
 
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
